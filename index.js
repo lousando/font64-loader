15be2afc47d3cb2e662c46dbcd577b84a949c253
Louis Sandoval <louis@sandoval.com>
2016-01-06 19:13:44 -0700

    Initial loader commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = function (content) {
+    this.cacheable && this.cacheable();
+    var encoded_font = content.toString('base64');
+    return `module.exports = 'data:application/font;charset=utf-8;base64,${encoded_font}'`;
+};
+
+module.exports.raw = true;
